148c31b7e4d1c15af56cf3463b3a83832454eb1b
Julien Cortial <jcortial@gmail.com>
2023-07-24 23:06:18 +0200

    Enable visitor pattern

--- lox/expr.py
+++ lox/expr.py
@@ -1,28 +1,36 @@
 from tokens import Token
-
-
 class Expr:
-    pass
-
-
+    def accept(self, visitor):
+        pass
 class Binary(Expr):
-    def __init__(self, left: Expr, operator: Token, right: Expr) -> None:
-        self.left: Expr = left
-        self.operator: Token = operator
-        self.right: Expr = right
-
-
+    def __init__(self, left: Expr, operator: Token, right: Expr):
+        self.left = left
+        self.operator = operator
+        self.right = right
+    def accept(self, visitor):
+        visitor.visit_binary_expr(self)
 class Grouping(Expr):
-    def __init__(self, expression: Expr) -> None:
-        self.expression: Expr = expression
-
-
+    def __init__(self, expression: Expr):
+        self.expression = expression
+    def accept(self, visitor):
+        visitor.visit_grouping_expr(self)
 class Literal(Expr):
-    def __init__(self, value: object) -> None:
-        self.value: object = value
-
-
+    def __init__(self, value: object):
+        self.value = value
+    def accept(self, visitor):
+        visitor.visit_literal_expr(self)
 class Unary(Expr):
-    def __init__(self, operator: Token, right: Expr) -> None:
-        self.operator: Token = operator
-        self.right: Expr = right
+    def __init__(self, operator: Token, right: Expr):
+        self.operator = operator
+        self.right = right
+    def accept(self, visitor):
+        visitor.visit_unary_expr(self)
+class Visitor:
+    def visit_binary_expr(self, expr: Binary):
+        pass
+    def visit_grouping_expr(self, expr: Grouping):
+        pass
+    def visit_literal_expr(self, expr: Literal):
+        pass
+    def visit_unary_expr(self, expr: Unary):
+        pass
--- tool/generate_ast.py
+++ tool/generate_ast.py
@@ -28,12 +28,25 @@ def define_ast(output_dir: str, base_name: str, types: list[str]) -> None:
     with open(path, "w") as writer:
         writer.write(f"from tokens import Token\n")
         writer.write(f"class {base_name}:\n")
-        writer.write(f"{indent}pass\n")
+        # The base accept() method
+        writer.write(f"{indent}def accept(self, visitor):\n")
+        writer.write(f"{indent * 2}pass\n")
+        # The AST classes
         for type in types:
             data: list[str] = type.split("=>")
             class_name: str = data[0].strip()
             fields: str = data[1].strip()
             define_type(writer, base_name, class_name, fields)
+        define_visitor(writer, base_name, types)
+
+def define_visitor(writer: io.TextIOBase, base_name: str, types: list[str]):
+    writer.write(f"class Visitor:\n")
+    for type in types:
+        type_name: str = type.split("=>")[0].strip()
+        writer.write(
+            f"{indent}def visit_{type_name.lower()}_{base_name.lower()}(self, {base_name.lower()}: {type_name}):\n"
+        )
+        writer.write(f"{indent * 2}pass\n")
 
 
 def define_type(
@@ -46,6 +59,9 @@ def define_type(
     for field in field_list.split(","):
         name: str = field.split(":")[0].strip()
         writer.write(f"{indent * 2}self.{name} = {name}\n")
+    # Visitor pattern
+    writer.write(f"{indent}def accept(self, visitor):\n")
+    writer.write(f"{indent * 2}visitor.visit_{class_name.lower()}_{base_name.lower()}(self)\n")
 
 
 if __name__ == "__main__":
